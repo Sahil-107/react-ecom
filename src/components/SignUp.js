c21d1fb8b2e594267feecf5d2f53261e913e3b01
Sahil-107 <messisahil7@gmail.com>
2024-08-29 15:30:25 +0530

    signup working

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -18,41 +18,58 @@ function SignUp() {
     setFormValues({ ...formValues, [name]: value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    setFormErrors(validate(formValues));
-    setIsSubmit(true);
+    // setFormErrors(validate(formValues));
+    try {
+      const response = await fetch("http://localhost:8080/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formValues),
+      });
+      if (response.ok) {
+        const data = await response.json();
+        console.log("User signedup successfully", data);
+      } else {
+        const errorData = await response.json();
+        console.log(errorData);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-  useEffect(() => {
-    console.log(formErrors);
-    if (Object.keys(formErrors).length === 0 && isSubmit) {
-      console.log(formValues);
-    }
-  }, [formErrors, formValues, isSubmit]);
-  const validate = (values) => {
-    const errors = {};
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-    if (!values.username) {
-      errors.username = "Username is required!";
-    }
-    if (!values.email) {
-      errors.email = "Email is required!";
-    } else if (!regex.test(values.email)) {
-      errors.email = "This is not a valid email format!";
-    }
-    if (!values.password) {
-      errors.password = "Password is required";
-    } else if (values.password.length < 4) {
-      errors.password = "Password must be more than 4 characters";
-    } else if (values.password.length > 10) {
-      errors.password = "Password cannot exceed more than 10 characters";
-    }
-    if (values.password !== values.confirmPassword) {
-      errors.confirmPassword = "Those passwords didn’t match. Try again.";
-    }
-    return errors;
-  };
+  // useEffect(() => {
+  //   console.log(formErrors);
+  //   if (Object.keys(formErrors).length === 0 && isSubmit) {
+  //     console.log(formValues);
+  //   }
+  // }, [formErrors, formValues, isSubmit]);
+  // const validate = (values) => {
+  //   const errors = {};
+  //   const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+  //   if (!values.username) {
+  //     errors.username = "Username is required!";
+  //   }
+  //   if (!values.email) {
+  //     errors.email = "Email is required!";
+  //   } else if (!regex.test(values.email)) {
+  //     errors.email = "This is not a valid email format!";
+  //   }
+  //   if (!values.password) {
+  //     errors.password = "Password is required";
+  //   } else if (values.password.length < 4) {
+  //     errors.password = "Password must be more than 4 characters";
+  //   } else if (values.password.length > 10) {
+  //     errors.password = "Password cannot exceed more than 10 characters";
+  //   }
+  //   if (values.password !== values.confirmPassword) {
+  //     errors.confirmPassword = "Those passwords didn’t match. Try again.";
+  //   }
+  //   return errors;
+  // };
 
   return (
     <>
